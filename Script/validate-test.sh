c81160aed0c087fb59f825faa23e6f574db3a2ba
Espen Stranger Seland <espen@seland.org>
2020-10-15 20:37:36 +0200

    Update validate-test.sh

--- Script/validate-test.sh
+++ Script/validate-test.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 # Plan: Validate full repo with Torinox/.net
+printf "FHIR, walk with me!\n"
+# Torinox støtter ennå ikke bulk-validering
+fhir4 validate ../StructureDefinition/no-basis-Address.structuredefinition-profile.xml
+# TODO Fikse login på Simplifier.net
